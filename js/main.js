5717aebf39398d7e7639b8c43562a1cd59001cf8
kihunhan <kihunhan@gmail.com>
2023-06-01 21:51:46 +0900

    공통 모듈 분리

--- js/main.js
+++ js/main.js
@@ -1,34 +1,12 @@
-
-const searchEl = document.querySelector('.search');
-const searchInputEl = searchEl.querySelector('input');
-
-searchEl.addEventListener('click', function(){
-  //Logic..
-  searchInputEl.focus();
-});
-
-searchInputEl.addEventListener('focus', function() {
-  searchEl.classList.add('focused');
-  searchInputEl.setAttribute('placeholder', '통합검색');
-});
-
-
-searchInputEl.addEventListener('blur', function() {
-  searchEl.classList.remove('focused');
-  searchInputEl.setAttribute('placeholder', '');
-});
-
-
 const badgeEl = document.querySelector('header .badges');
 const toTopEl = document.querySelector('#to-top');
-
 
 window.addEventListener('scroll', _.throttle( function() {
   console.log(window.scrollY);
   if (window.scrollY > 500) {
     //배지 숨기기
-    // badgeEl.style.display = 'none';
-    // gsap.to(요소, 지속시간, 옵션); 
+    // badgeEl.style.display = 'none';  
+    // gsap.to(요소, 지속시간, 옵션);   
     gsap.to(badgeEl, .6, {
       opacity: 0,
       display: 'none'
@@ -160,8 +138,7 @@ spyEls.forEach(function (spyEl) {
 });
 
 
-const thisYear = document.querySelector('.this-year');
-thisYear.textContent = new Date().getFullYear();
+
 
 
 
